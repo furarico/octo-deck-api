88414e2151bc5a141f00a6e25e5222ab93c1db39
Hikaru Saito <beretta0602@gmail.com>
2025-12-15 17:12:42 +0900

    メソッド切り分けと命名修正

---
+++ internal/handler/get_my_card.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// (GET /cards/me)
+func (h *Handler) GetMyCard(c *gin.Context) {
+	card, err := h.cardService.GetMyCard()
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"card": card,
+	})
+}
